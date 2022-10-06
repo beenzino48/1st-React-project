ccfde73f4ca4be7dfb6354a5ab661ccb61a28f52
hamishliu <hamish.mc.liu@gmail.com>
2022-10-06 15:57:27 +0900

    added 4 components which are not hardcoded but have JS values written in the app file

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,12 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>March 28th 2021</div>
+      <div>{props.date.toDateString()}</div>
       <div className="expense-item-description">
-        <h2>Car insurance</h2>
-        <div className="expense-item__price">$289</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
