84252285ae9adf3e5ae3975418c18777acd0b033
hamishliu <hamish.mc.liu@gmail.com>
2022-10-07 17:31:49 +0900

    added an event for when the button is clicked

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,17 +3,20 @@ import "./ExpenseItem.css";
 import Card from "./Card";
 
 function ExpenseItem(props) {
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const year = props.date.getFullYear();
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-
+  // function for button click
+  const clickHandler = () => {
+    console.log("clicked!");
+  };
   return (
     <div className="expense-item">
+      {/* data comes from ExpenseDate.js */}
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      {/* button changes the h2 title */}
+      <button onClick={clickHandler}>Change title</button>
     </div>
   );
 }
