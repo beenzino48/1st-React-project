76b05e085f3985158df143d5d8ec3d21597b467c
hamishliu <hamish.mc.liu@gmail.com>
2022-10-08 16:49:12 +0900

    added state changes for changing the title, amount and date.

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,18 +1,62 @@
-import React from "react";
+import React, { UserState, useState } from "react";
 
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+  // State set for user's input change
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+  // Uses an object to set all three states in one const
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: "",
+  //   enteredAmount: "",
+  //   enteredDate: "",
+  // });
+  // set function to get user's input
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle: event.target.value,
+    // });
+    // This approach guarantees that you will receive the latest state each time
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredTitle: event.target.value };
+    // });
+  };
+
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredAmount: event.target.value,
+    // });
+  };
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredDate: event.target.value,
+    // });
+  };
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text"></input>
+          {/* Added listener for users input */}
+          <input type="text" on onChange={titleChangeHandler}></input>
         </div>
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="number" min="0.01" step="0.01"></input>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler}
+          ></input>
         </div>
         <div className="new-expense__control">
           <label>Title</label>
