d987bca46ca9997a9ac9a1147dabd5ecc2d7f5e5
hamishliu <hamish.mc.liu@gmail.com>
2022-10-13 07:31:33 +0900

    added a button to show expenses form

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 // This gets users input from ExpenseForm  title, amount and date
+
 const NewExpense = (props) => {
+  const [changeText, setChangeText] = useState(true);
+
+  const handleChange = () => {
+    return setChangeText(!changeText);
+  };
+
+  const cancelHandle = () => {
+    return setChangeText(changeText);
+  };
+
   // this function takes the expenseData object
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
@@ -15,10 +26,16 @@ const NewExpense = (props) => {
     // allows the result to be seen in app.js
     props.onAddExpense(expenseData);
   };
+
   return (
     <div className="new-expense">
       {/* Getting data from expenseForm */}
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      <button onClick={() => handleChange()}>Add Expense</button>
+      {changeText ? (
+        ""
+      ) : (
+        <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      )}
     </div>
   );
 };
